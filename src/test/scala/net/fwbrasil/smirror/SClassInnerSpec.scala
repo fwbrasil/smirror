ce7ab4f3b3461ca6ae60caa92a3c8641521b782a
Flavio W. Brasil <fwbrasil@gmail.com>
2013-04-14 08:50:56 -0300

    minor

--- src/test/scala/net/fwbrasil/smirror/SClassInnerSpec.scala
+++ src/test/scala/net/fwbrasil/smirror/SClassInnerSpec.scala
@@ -1,25 +1,27 @@
 package net.fwbrasil.smirror
 
+import scala.reflect.runtime.currentMirror
+
 class SClassInnerSpecTestClass {
-	class InnerClass(val m1: Int)
-	def newInner = new InnerClass(1)
+    class InnerClass(val m1: Int)
+    def newInner = new InnerClass(1)
 }
 
 class SClassInnerSpec extends SMirrorSpec {
 
-	val instance = new SClassInnerSpecTestClass
-	val inner = instance.newInner
+    val instance = new SClassInnerSpecTestClass
+    val inner = instance.newInner
 
-	"A inner SClass" should "invoke constructor" in
-		test[SClassInnerSpecTestClass#InnerClass] { (sClass, jClass) =>
-			sClass.constructors.head.invoke(instance, 2).m1 should
-				equal(2)
-		}
+    "A inner SClass" should "invoke constructor" in
+        test[SClassInnerSpecTestClass#InnerClass] { (sClass, jClass) =>
+            sClass.constructors.head.invoke(instance, 2).m1 should
+                equal(2)
+        }
 
-	"A inner SClass" should "return val" in
-		test[SClassInnerSpecTestClass#InnerClass] { (sClass, jClass) =>
-			sClass.vals.head.get(inner) should
-				equal(1)
-		}
+    "A inner SClass" should "return val" in
+        test[SClassInnerSpecTestClass#InnerClass] { (sClass, jClass) =>
+            sClass.vals.head.get(inner) should
+                equal(1)
+        }
 
 }
