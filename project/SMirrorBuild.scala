cba318b816082c8cdd9a15aa3bad0a6f3d157051
Flavio W. Brasil <fwbrasil@gmail.com>
2014-09-20 18:27:09 +0200

    remove cross compilation

--- project/SMirrorBuild.scala
+++ project/SMirrorBuild.scala
@@ -3,58 +3,53 @@ import Keys._
 
 object SMirrorBuild extends Build {
 
-	/* Dependencies */
-	val scalaTest = "org.scalatest" %% "scalatest" % "2.2.1" % "test"
+    /* Dependencies */
+    val scalaTest = "org.scalatest" %% "scalatest" % "2.2.1" % "test"
 
-	def scalaReflect(scalaVersion: String) = "org.scala-lang" % "scala-reflect" % scalaVersion
+    val scalaReflect = "org.scala-lang" % "scala-reflect" % "2.11.2"
 
-	lazy val sMirror =
-		Project(
-			id = "sMirror",
-			base = file("."),
-			settings = Defaults.defaultSettings ++ Seq(
-				libraryDependencies ++= Seq(scalaTest),
-				libraryDependencies <+= scalaVersion(scalaReflect(_)),
-				// publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository"))), 
-				// publishTo := Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as ("maven") withPermissions ("0644")),
-				publishTo <<= version { v: String =>
-				  val nexus = "https://oss.sonatype.org/"
-				  val fwbrasil = "http://fwbrasil.net/maven/"
-				  if (v.trim.endsWith("SNAPSHOT")) 
-				    Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as("maven") withPermissions("0644"))
-				  else                             
-				    Some("releases" at nexus + "service/local/staging/deploy/maven2")
-				},
-				credentials += Credentials(Path.userHome / ".sbt" / "sonatype.credentials"),
-				publishMavenStyle := true,
-				publishArtifact in Test := false,
-				pomIncludeRepository := { x => false },
-				pomExtra := (
-				  <url>http://github.com/fwbrasil/smirror/</url>
-				  <licenses>
-				    <license>
-				      <name>LGPL</name>
-				      <url>https://github.com/fwbrasil/smirror/blob/master/LICENSE-LGPL</url>
-				      <distribution>repo</distribution>
-				    </license>
-				  </licenses>
-				  <scm>
-				    <url>git@github.com:fwbrasil/smirror.git</url>
-				    <connection>scm:git:git@github.com:fwbrasil/smirror.git</connection>
-				  </scm>
-				  <developers>
-				    <developer>
-				      <id>fwbrasil</id>
-				      <name>Flavio W. Brasil</name>
-				      <url>http://fwbrasil.net</url>
-				    </developer>
-				  </developers>
-				),
-				organization := "net.fwbrasil",
-				scalaVersion := "2.11.2",
-				crossScalaVersions := Seq("2.10.1", "2.11.2"),
-				version := "0.8"
-			)
-		)
+    lazy val sMirror =
+        Project(
+            id = "sMirror",
+            base = file("."),
+            settings = Defaults.defaultSettings ++ Seq(
+                libraryDependencies ++= Seq(scalaTest, scalaReflect),
+                // publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository"))), 
+                // publishTo := Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as ("maven") withPermissions ("0644")),
+                publishTo <<= version { v: String =>
+                    val nexus = "https://oss.sonatype.org/"
+                    val fwbrasil = "http://fwbrasil.net/maven/"
+                    if (v.trim.endsWith("SNAPSHOT"))
+                        Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as ("maven") withPermissions ("0644"))
+                    else
+                        Some("releases" at nexus + "service/local/staging/deploy/maven2")
+                },
+                credentials += Credentials(Path.userHome / ".sbt" / "sonatype.credentials"),
+                publishMavenStyle := true,
+                publishArtifact in Test := false,
+                pomIncludeRepository := { x => false },
+                pomExtra := (
+                    <url>http://github.com/fwbrasil/smirror/</url>
+                    <licenses>
+                        <license>
+                            <name>LGPL</name>
+                            <url>https://github.com/fwbrasil/smirror/blob/master/LICENSE-LGPL</url>
+                            <distribution>repo</distribution>
+                        </license>
+                    </licenses>
+                    <scm>
+                        <url>git@github.com:fwbrasil/smirror.git</url>
+                        <connection>scm:git:git@github.com:fwbrasil/smirror.git</connection>
+                    </scm>
+                    <developers>
+                        <developer>
+                            <id>fwbrasil</id>
+                            <name>Flavio W. Brasil</name>
+                            <url>http://fwbrasil.net</url>
+                        </developer>
+                    </developers>),
+                organization := "net.fwbrasil",
+                scalaVersion := "2.11.2",
+                version := "0.8"))
 
 }
